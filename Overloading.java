3532c69ed3619c12d528952fc876d0de92d4e92d
Gurjot singh <harjoth200@gmail.com>
2026-05-16 11:32:56 +0530

    overloading

---
+++ Overloading.java
@@ -0,0 +1,20 @@
+class Addition {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class Overloading {
+    public static void main(String[] args) {
+
+        Addition obj = new Addition();
+
+        System.out.println(obj.add(10, 20));
+        System.out.println(obj.add(10, 20, 30));
+    }
+}
